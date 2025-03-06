b008f1e019d8523f57e8c24eb0112ede444e4cdc
jagan1508 <102706723+jagan1508@users.noreply.github.com>
2025-03-06 18:17:56 +0530

    modified app.py

--- src2/app.py
+++ src2/app.py
@@ -1,5 +1,8 @@
 from graph import graph
 from typing import Union
+import sys
+from pydantic import BaseModel
+
 
 from fastapi import FastAPI
 import uvicorn
@@ -9,9 +12,21 @@ result=graph.invoke({"question":"I like whales","max_attempts":2},config=config)
 print("answer is ",result["answer"])"""
 app = FastAPI()
 
+class QuestionRequest(BaseModel):
+    text: str
+
 @app.post("/ask")
-def ask(text: str):
+def ask(request: QuestionRequest):
     config = {"configurable": {"thread_id": "1"}}
-    result= graph.invoke({"question":text,"max_attempts":2},config=config)
+    result= graph.invoke({"question":request.text,"max_attempts":2},config=config)
     #print("This is the result :",result)
-    return result["answer"]
+    return {"answer":result["answer"]}
+
+def main(argv=sys.argv[1:]):
+    try:
+        uvicorn.run("app:app", host="127.0.0.1", port=3001)
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    main()
